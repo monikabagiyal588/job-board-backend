c4b8d3af48f2f1b522a181bf7b6159fbeab01d00
Monika Bagiyal <monikabagiyal588>
2025-07-14 14:21:47 +0530

    package.json commit

--- config/db.config.js
+++ config/db.config.js
@@ -4,6 +4,12 @@ module.exports = {
   USER: process.env.DB_USERNAME,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
-  dialect: "mysql"
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
 
